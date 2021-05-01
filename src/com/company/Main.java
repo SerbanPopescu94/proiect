89be25f03d649d4fcbd8750d3f16066f6ba02a1d
Serban <serban.m.popescu2@gmail.com>
2021-05-01 13:44:09 +0300

    Last modify TODO

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Map<String,Integer> ageOfFriends = Map.of("Serban",25,"Vlad",30,"Andrei",20);
+
+
+
+        for(Map.Entry<String,Integer> entry: ageOfFriends.entrySet()){
+            String friend = entry.getKey();
+            Integer age = entry.getValue();
+            System.out.println(friend + " is " + age + " years old ");
+        }
+
+        List<String> stringList =Arrays.asList("P","O","P","E","S","C","U");
+
+        String result =stringList.stream().collect(Collectors.joining());
+        System.out.println(result);
+        /////last modify
+    }
+}
