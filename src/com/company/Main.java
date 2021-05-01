b603ee4378194e28b3be23283b71c86b7fbcd795
Serban <serban.m.popescu2@gmail.com>
2021-05-01 13:58:21 +0300

    Last modify TODO

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,7 +9,7 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static void main(String[] args) {
-        Map<String,Integer> ageOfFriends = Map.of("Serban",25,"Vlad",30,"Andrei",20);
+        /*Map<String,Integer> ageOfFriends = Map.of("Serban",25,"Vlad",30,"Andrei",20);
 
 
 
@@ -17,12 +17,20 @@ public class Main {
             String friend = entry.getKey();
             Integer age = entry.getValue();
             System.out.println(friend + " is " + age + " years old ");
-        }
+        }*/
 
-        List<String> stringList =Arrays.asList("P","O","P","E","S","C","U");
+        List<String> stringList = Arrays.asList("P", "O", "P", "E", "S", "C", "U");
 
-        String result =stringList.stream().collect(Collectors.joining());
+        String result = stringList.stream().collect(Collectors.joining());
         System.out.println(result);
         /////last modify
+
+        Map<String, Double> weightOfFriends = Map.of("Vlad", 55.4, "Serban", 84.2);
+
+        for (Map.Entry<String, Double> entry: weightOfFriends.entrySet()) {
+            String friend = entry.getKey();
+            Double greutate = entry.getValue();
+            System.out.println(friend + " are " + greutate + "kg" );
+        }
     }
 }
